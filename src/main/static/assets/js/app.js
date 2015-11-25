3e0aaf7516eb24d5c5a98ea16a50b46e71c25631
Damien Vitrac <dvitrac@doc4web.com>
2015-11-25 03:37:12 +0000

    Mobile adaptation

--- src/main/static/assets/js/app.js
+++ src/main/static/assets/js/app.js
@@ -16,4 +16,24 @@
 
 $(function(){
     // Main JS
+    var navVisible = false;
+    var toggle_el = $("[data-toggle]").data('toggle');
+
+    $("[data-toggle]").click(function() {
+        $(toggle_el).toggleClass("open-sidebar");
+        navVisible = !navVisible;
+    });
+
+    $("#content").click(function(){
+        if (navVisible) {
+            navVisible = false;
+            $(toggle_el).removeClass("open-sidebar");
+        }
+    });
+    $(window).resize(function(){
+        if ($(this).width() > 800 && navVisible) {
+            navVisible = false;
+            $(toggle_el).removeClass("open-sidebar");
+        }
+    })
 });
