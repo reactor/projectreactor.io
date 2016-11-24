32ef275f01b76c44e5f3a20801ee88e3f07b35b5
Stephane Maldini <smaldini@pivotal.io>
2016-11-24 22:12:52 +0000

    try fix for CF site

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,34 +10,33 @@ import java.nio.ByteBuffer
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 
-val artifacts= listOf(
+val artifacts = listOf(
         Triple("io.projectreactor", "reactor-core", "3.0.3.RELEASE"),
         Triple("io.projectreactor.addons", "reactor-test", "3.0.3.RELEASE"),
         Triple("io.projectreactor.addons", "reactor-adapter", "3.0.3.RELEASE"),
-        Triple("io.projectreactor.ipc", "reactor-netty", "0.5.2.RELEASE")
+        Triple("io.projectreactor.ipc", "reactor-netty", "0.6.0.BUILD-SNAPSHOT")
 )
 
 buildscript {
-
-  repositories {
-    mavenLocal()
-    jcenter()
-    maven { setUrl("http://dl.bintray.com/robfletcher/gradle-plugins") }
-    maven { setUrl("https://repo.spring.io/snapshot") }
-    mavenCentral()
-  }
-  dependencies {
-    classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.BUILD-SNAPSHOT")
-    classpath("com.github.robfletcher:compass-gradle-plugin:2.0.6")
-    classpath("org.springframework:spring-web:5.0.0.BUILD-SNAPSHOT")
-    classpath("io.projectreactor.ipc:reactor-netty:0.6.0.BUILD-SNAPSHOT")
-  }
+    repositories {
+        mavenLocal()
+        jcenter()
+        maven { setUrl("http://dl.bintray.com/robfletcher/gradle-plugins") }
+        maven { setUrl("https://repo.spring.io/snapshot") }
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:2.0.0.BUILD-SNAPSHOT")
+        classpath("com.github.robfletcher:compass-gradle-plugin:2.0.6")
+        classpath("org.springframework:spring-web:5.0.0.BUILD-SNAPSHOT")
+        classpath("io.projectreactor.ipc:reactor-netty:0.6.0.BUILD-SNAPSHOT")
+    }
 }
 
 apply {
-  plugin("java")
-  plugin("com.github.robfletcher.compass")
-  plugin("org.springframework.boot")
+    plugin("java")
+    plugin("com.github.robfletcher.compass")
+    plugin("org.springframework.boot")
 }
 
 group = "io.projectreactor"
@@ -53,23 +52,26 @@ configure<SpringBootPluginExtension> {
 }
 
 configure<CompassExtension> {
-  sassDir = file("$projectDir/src/main/sass")
-  cssDir = file("$buildDir/resources/main/static/assets/css")
+    sassDir = file("$projectDir/src/main/sass")
+    cssDir = file("$buildDir/resources/main/static/assets/css")
 }
 
 
 repositories {
-  mavenCentral()
-  maven { setUrl("http://repo.spring.io/libs-milestone") }
-  maven { setUrl("https://repo.spring.io/snapshot") }
+    mavenLocal()
+    mavenCentral()
+    maven { setUrl("http://repo.spring.io/libs-milestone") }
+    maven { setUrl("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-  compile("org.springframework:spring-web-reactive:5.0.0.BUILD-SNAPSHOT")
-  // TODO Remove the spring-context-support dependency when https://jira.spring.io/browse/SPR-14908 will be fixed
-  compile("org.springframework:spring-context-support:5.0.0.BUILD-SNAPSHOT")
-  compile("io.projectreactor.ipc:reactor-netty:0.6.0.BUILD-SNAPSHOT")
-  runtime("commons-logging:commons-logging:1.2")
+    compile("org.springframework:spring-web-reactive:5.0.0.BUILD-SNAPSHOT")
+    // TODO Remove the spring-context-support dependency when https://jira.spring.io/browse/SPR-14908 will be fixed
+    compile("org.springframework:spring-context-support:5.0.0.BUILD-SNAPSHOT")
+    compile("io.projectreactor.ipc:reactor-netty:0.6.0.BUILD-SNAPSHOT")
+    runtime("commons-logging:commons-logging:1.2")
+    runtime("org.slf4j:slf4j-api:1.7.21")
+    runtime("ch.qos.logback:logback-classic:1.1.7")
 }
 
 val docsGenerate = task("docsGenerate") {
@@ -95,24 +97,24 @@ val docsGenerate = task("docsGenerate") {
                     .map { it.asInputStream() }
                     .doOnError { println("Error for $artifactId-$version-javadoc.jar: $it") }
                     .block()
-             unzip(inputStream, "$outputDir/${artifactId.replace("reactor-", "")}/docs/api/")
+            unzip(inputStream, "$outputDir/${artifactId.replace("reactor-", "")}/docs/api/")
         }
     }
 }
 
-fun unzip(zip: InputStream, outputDir: String) : Unit {
-    val destDir:File = File(outputDir)
+fun unzip(zip: InputStream, outputDir: String): Unit {
+    val destDir: File = File(outputDir)
     if (destDir.exists()) destDir.deleteRecursively() else destDir.mkdirs()
     val zipIn: ZipInputStream = ZipInputStream(zip)
     var entry: ZipEntry? = zipIn.nextEntry
     while (entry != null) {
-        val filePath:String = outputDir + File.separator + entry.name
+        val filePath: String = outputDir + File.separator + entry.name
         if (!entry.isDirectory) {
-            val bos:BufferedOutputStream = BufferedOutputStream(FileOutputStream(filePath))
+            val bos: BufferedOutputStream = BufferedOutputStream(FileOutputStream(filePath))
             bos.write(zipIn.readBytes())
             bos.close()
         } else {
-            val dir:File = File(filePath)
+            val dir: File = File(filePath)
             dir.mkdirs()
         }
         zipIn.closeEntry()
