77d4e92f4bd4821e45a83fc06f70e1202e2eba2c
Stephane Maldini <smaldini@pivotal.io>
2017-02-10 14:59:29 +0000

    Try twitter feed in Learn Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
     dependencies {
         classpath("com.github.jengelman.gradle.plugins:shadow:1.2.4")
         classpath("com.github.robfletcher:compass-gradle-plugin:2.0.6")
-        classpath("io.projectreactor.ipc:reactor-netty:0.6.0.RELEASE")
+        classpath("io.projectreactor.ipc:reactor-netty:0.6.1.BUILD-SNAPSHOT")
     }
 }
 
@@ -53,13 +53,15 @@ configure<CompassExtension> {
 repositories {
     mavenLocal()
     mavenCentral()
-    maven { setUrl("http://repo.spring.io/libs-milestone") }
+    maven { setUrl("http://repo.spring.io/release") }
+    maven { setUrl("http://repo.spring.io/milestone") }
     maven { setUrl("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
     compile("org.springframework:spring-core:5.0.0.BUILD-SNAPSHOT")
     compile("io.projectreactor.ipc:reactor-netty:0.6.1.BUILD-SNAPSHOT")
+    compile("io.projectreactor:reactor-core:3.0.5.RELEASE")
     compile("org.yaml:snakeyaml:1.17")
     runtime("commons-logging:commons-logging:1.2")
     runtime("org.slf4j:slf4j-api:1.7.21")
