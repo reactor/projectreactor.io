099645ed017cea8308b63186abb253494807238f
Stephane Maldini <smaldini@pivotal.io>
2019-02-12 14:33:21 -0800

    Update gradle to 5.2 Update plugins definitions Update JDK support to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,21 @@
  * limitations under the License.
  */
 /*
+ * Copyright (c) 2011-Present Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -54,45 +69,22 @@ configurations.all {
     resolutionStrategy.force("org.jruby:jruby-complete:9.2.5.0")
 }
 
-buildscript {
-    repositories {
-        mavenLocal()
-        jcenter()
-        maven { setUrl("http://dl.bintray.com/robfletcher/gradle-plugins") }
-        maven { setUrl("https://repo.spring.io/release") }
-        maven { setUrl("https://repo.spring.io/snapshot") }
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.github.jengelman.gradle.plugins:shadow:1.2.4")
-        classpath("com.github.robfletcher:compass-gradle-plugin:2.0.6")
-        //NOTE: compass-gradle-plugin seems to somehow induce a dependency on JRuby/Ruby version < 2.2
-        //this causes the rb-inotify gem to fail to install in version 0.10.0 (because it now requires Ruby 2.2)
-        //see the `resolutionStrategy.force` above for the workaround, because bumping JRuby here didn't work
-    }
-}
-
-apply {
-    plugin("java")
-    plugin("com.github.robfletcher.compass")
-    plugin("application")
-    plugin("com.github.johnrengelman.shadow")
+plugins {
+    java
+    application
+    id("com.github.johnrengelman.shadow") version "4.0.4"
+    id("com.github.anbuck.compass") version "2.0.7"
 }
 
 group = "io.projectreactor"
 version = "1.0.0.BUILD-SNAPSHOT"
-
-configure<JavaPluginConvention> {
-    setSourceCompatibility(1.8)
-    setTargetCompatibility(1.8)
-}
 
 configure<ApplicationPluginConvention> {
 	mainClassName = "io.projectreactor.Application"
 }
 
 configure<ShadowExtension> {
-	version = null
+	version = ""
 }
 
 configure<CompassExtension> {
