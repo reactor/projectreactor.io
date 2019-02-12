0ca0d0709a8d747d6623a735e80737a3330a2431
Stephane Maldini <smaldini@pivotal.io>
2019-02-12 12:49:19 -0800

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,19 @@
 /*
+ * Copyright (c) 2011-Present Pivotal Software Inc, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Copyright (c) 2011-2018 Pivotal Software Inc, All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -94,8 +109,8 @@ repositories {
 
 dependencies {
     compile("org.springframework:spring-core:5.1.3.RELEASE")
-    compile("io.projectreactor.netty:reactor-netty:0.8.4.BUILD-SNAPSHOT")
-    compile("io.projectreactor:reactor-core:3.2.3.RELEASE")
+    compile("io.projectreactor.netty:reactor-netty:0.8.6.BUILD-SNAPSHOT")
+    compile("io.projectreactor:reactor-core:3.2.6.RELEASE")
     compile("org.thymeleaf:thymeleaf:3.0.9.RELEASE")
     compile("org.yaml:snakeyaml:1.17")
     runtime("commons-logging:commons-logging:1.2")
