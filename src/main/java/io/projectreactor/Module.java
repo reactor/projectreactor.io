bd83cb8dcfba048b36f6f5e1be315c73c34538b7
Simon Baslé <sbasle@pivotal.io>
2019-11-21 18:49:23 +0100

    [polish] Make Module regexp and comparator understand customVersions

--- src/main/java/io/projectreactor/Module.java
+++ src/main/java/io/projectreactor/Module.java
@@ -90,23 +90,30 @@ public class Module {
 		return "Module{" + "name='" + name + '\'' + ", groupId='" + groupId + '\'' + ", artifactId='" + artifactId + '\'' + ", versions=" + versions + '}';
 	}
 
-	static final Pattern VERSION_REGEXP = Pattern.compile("[0-9]+\\.[0-9]+\\.[0-9]+\\.[a-zA-Z0-9_-]*");
+	/**
+	 * A regexp that matches version numbers in the form of either {@code GEN.MAJOR.MINOR.qualifier} or
+	 * {@code GEN.MAJOR.MINOR.CustomVersion.qualifier}. {@code CustomVersion} is expected to be alphanumerical only.
+	 */
+	static final Pattern VERSION_REGEXP = Pattern.compile("[0-9]+\\.[0-9]+\\.[0-9]+(\\.[a-zA-Z0-9]*)?\\.[a-zA-Z0-9_-]*");
+
 	static final Comparator<String> VERSION_COMPARATOR = (o1, o2) -> {
 		String[] o1Split = o1.split("\\.");
 		String[] o2Split = o2.split("\\.");
-		boolean o1IsVersion = o1Split.length == 4;
-		boolean o2IsVersion = o2Split.length == 4;
+		boolean o1IsVersion = (o1Split.length == 4 || o1Split.length == 5);
+		boolean o2IsVersion = (o2Split.length == 4 || o2Split.length == 5);
 
 		if (o1IsVersion && o2IsVersion) {
 			int o1X= Integer.parseInt(o1Split[0]);
 			int o1Y = Integer.parseInt(o1Split[1]);
 			int o1Z = Integer.parseInt(o1Split[2]);
-			String o1Qualifier = o1Split[3];
+			String o1Qualifier = o1Split.length == 4 ? o1Split[3] : o1Split[4];
+			String o1CustomVersion = o1Split.length == 4 ? "" : o1Split[3];
 
 			int o2X= Integer.parseInt(o2Split[0]);
 			int o2Y = Integer.parseInt(o2Split[1]);
 			int o2Z = Integer.parseInt(o2Split[2]);
-			String o2Qualifier = o2Split[3];
+			String o2Qualifier = o2Split.length == 4 ? o2Split[3] : o2Split[4];
+			String o2CustomVersion = o2Split.length == 4 ? "" : o2Split[3];
 
 			if (o1X != o2X) {
 				return o2X - o1X;
@@ -117,7 +124,11 @@ public class Module {
 			if (o1Z != o2Z) {
 				return o2Z - o1Z;
 			}
-			return o2Qualifier.compareTo(o1Qualifier);
+			if (!o1Qualifier.equals(o2Qualifier)) {
+				return o2Qualifier.compareTo(o1Qualifier);
+			}
+			//custom version are in natural order rather than reverse order
+			return o1CustomVersion.compareTo(o2CustomVersion);
 		}
 
 		if (!o1IsVersion && !o2IsVersion) return o1.compareTo(o2);
--- src/test/java/io/projectreactor/ModuleTest.java
+++ src/test/java/io/projectreactor/ModuleTest.java
@@ -49,4 +49,53 @@ public class ModuleTest {
 		assertThat(versions).containsExactly("3.2.0.RELEASE", "2.2.0.RELEASE", "1.2.0.RELEASE");
 	}
 
+	@Test
+	public void compareCustomVersionSameQualifier() {
+		List<String> versions = Arrays.asList("1.2.3.customVersionA.BUILD-SNAPSHOT", "1.2.3.BUILD-SNAPSHOT", "1.2.3.customVersionB.BUILD-SNAPSHOT");
+		versions.sort(Module.VERSION_COMPARATOR);
+
+		assertThat(versions).containsExactly("1.2.3.BUILD-SNAPSHOT", "1.2.3.customVersionA.BUILD-SNAPSHOT", "1.2.3.customVersionB.BUILD-SNAPSHOT");
+	}
+
+	@Test
+	public void matchesReleaseVersion() {
+		assertThat("1.2.3.RELEASE").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void matchesReleaseCandidateVersion() {
+		assertThat("1.2.3.RC11").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void matchesMilestoneVersion() {
+		assertThat("1.2.3.M23").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void matchesSnapshotVersion() {
+		assertThat("1.2.3.BUILD-SNAPSHOT").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void matchesCustomSnapshotVersions() {
+		assertThat("1.2.3.01.BUILD-SNAPSHOT").as("numeric only").matches(Module.VERSION_REGEXP);
+		assertThat("1.2.3.0cusTom0.BUILD-SNAPSHOT").as("alphanumeric").matches(Module.VERSION_REGEXP);
+		assertThat("1.2.3.cusTom.BUILD-SNAPSHOT").as("alphabetic only").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void matchesCustomReleaseVersions() {
+		assertThat("1.2.3.01.RELEASE").as("numeric only").matches(Module.VERSION_REGEXP);
+		assertThat("1.2.3.0cusTom0.RELEASE").as("alphanumeric").matches(Module.VERSION_REGEXP);
+		assertThat("1.2.3.cusTom.RELEASE").as("alphabetic only").matches(Module.VERSION_REGEXP);
+	}
+
+	@Test
+	public void doesntMatchNonAlphaCustom() {
+		assertThat("1.2.3.0-1.RELEASE").as("numeric only").doesNotMatch(Module.VERSION_REGEXP);
+		assertThat("1.2.3.0cus-Tom0.RELEASE").as("alphanumeric").doesNotMatch(Module.VERSION_REGEXP);
+		assertThat("1.2.3.cus-Tom.RELEASE").as("alphabetic only").doesNotMatch(Module.VERSION_REGEXP);
+	}
+
 }
